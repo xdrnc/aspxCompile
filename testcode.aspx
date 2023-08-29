a0679026f67f481f93dd52ec000cc5a6cdb118f3
alexanderng <alex@sitecore.net>
2023-08-29 10:14:48 -0400

    working base with code sample, require admin user

---
+++ testcode.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" Debug="true" CodeFile="testcode.aspx.cs" inherits="TestCode"%>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <textarea id="TextArea1" name="TextArea1" runat="server" cols="150" rows="20"></textarea>
+        <asp:Button ID="Button1" OnClick="execute_Click" runat="server" Text="Execute" />
+        <textarea id="TextArea2" runat="server" cols="150" rows="20" ></textarea>
+    </div>
+    </form>
+</body>
+</html>
---
+++ testcode.aspx.cs
@@ -0,0 +1,111 @@
+﻿using System.CodeDom.Compiler;
+using System.Diagnostics;
+using Microsoft.CSharp;
+using Sitecore.Data;
+using Sitecore.Diagnostics;
+using Sitecore.Web.UI.Controls.Common.TextAreas;
+using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Linq;
+
+public partial class TestCode : System.Web.UI.Page
+{
+    private string codeSample = @"
+using System;
+
+namespace SitecoreTestCode
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(""code sample..."");
+        }
+    }
+}
+    ";
+
+    //search in cache values 
+    private string pre1 = @"";
+
+    //search in content index
+    private string pre2 = @"";
+
+    //GetItem request
+    private string pre3 = @"";
+
+    //load recent events and last processed timestamp
+    private string pre4 = @"";
+
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        TextArea1.InnerText = codeSample;
+    }
+
+    //    protected string Result { get; set; }
+    protected void execute_Click(object sender, EventArgs e)
+    {
+        // check user must have administrator role
+        if (Sitecore.Context.User.IsAdministrator)
+        {
+            string input = Request.Form["TextArea1"];
+
+            TextArea1.InnerText = input;
+
+            string result = "";
+
+            CSharpCodeProvider csp = new CSharpCodeProvider();
+            ICodeCompiler icc = csp.CreateCompiler();
+
+            CompilerParameters compParams = new CompilerParameters();
+            compParams.GenerateExecutable = true;
+            compParams.GenerateInMemory = true;
+
+            string outputLocation = AppContext.BaseDirectory + "out.exe";
+            compParams.OutputAssembly = outputLocation;
+
+            CompilerResults compResults = icc.CompileAssemblyFromSource(compParams, input);
+
+            if (compResults.Errors.HasErrors)
+            {
+                compResults.Errors.Cast<CompilerError>().ToList().ForEach(er => Response.Write(er + "\n"));
+            }
+            else
+            {
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = outputLocation,
+                        Arguments = "command line arguments to your executable",
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        CreateNoWindow = true
+                    }
+                };
+
+                process.Start();
+                while (!process.StandardOutput.EndOfStream)
+                {
+                    result = process.StandardOutput.ReadLine();
+                };
+
+                TextArea2.InnerText = result;
+
+            }
+
+
+
+
+        }
+        else
+        {
+            Response.Write("Only administrator user can execute this");
+        }
+    }
+
+
+    }
+}
